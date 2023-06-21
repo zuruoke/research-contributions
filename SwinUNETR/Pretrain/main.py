7b991eb575b2be33dbc1e0a9ecf81a2c6c012cd7
zuruoke <zuruokeokafor2016@gmail.com>
2023-06-21 23:49:52 +0100

    edit file

--- SwinUNETR/Pretrain/main.py
+++ SwinUNETR/Pretrain/main.py
@@ -52,7 +52,8 @@ def main():
                 rots = torch.cat([rot1, rot2], dim=0)
                 imgs_recon = torch.cat([rec_x1, rec_x2], dim=0)
                 imgs = torch.cat([x1, x2], dim=0)
-                loss, losses_tasks = loss_function(rot_p, rots, contrastive1_p, contrastive2_p, imgs_recon, imgs)
+                loss, losses_tasks = loss_function(
+                    rot_p, rots, contrastive1_p, contrastive2_p, imgs_recon, imgs)
             loss_train.append(loss.item())
             loss_train_recon.append(losses_tasks[2].item())
             if args.amp:
@@ -62,7 +63,8 @@ def main():
             else:
                 loss.backward()
                 if args.grad_clip:
-                    torch.nn.utils.clip_grad_norm_(model.parameters(), args.max_grad_norm)
+                    torch.nn.utils.clip_grad_norm_(
+                        model.parameters(), args.max_grad_norm)
                 optimizer.step()
 
             if args.lrdecay:
@@ -70,25 +72,35 @@ def main():
             optimizer.zero_grad()
             if args.distributed:
                 if dist.get_rank() == 0:
-                    print("Step:{}/{}, Loss:{:.4f}, Time:{:.4f}".format(global_step, args.num_steps, loss, time() - t1))
+                    print("Step:{}/{}, Total Loss:{:.4f}, Rotation Loss:{:.4f}, Contrastive Loss:{:.4f}, Reconstruction Loss:{:.4f}, Time:{:.4f}".format(global_step,
+                          args.num_steps, loss, losses_tasks[0].item(), losses_tasks[1].item(), losses_tasks[2].item(), time() - t1))
             else:
-                print("Step:{}/{}, Loss:{:.4f}, Time:{:.4f}".format(global_step, args.num_steps, loss, time() - t1))
+                print("Step:{}/{}, Loss:{:.4f}, Rotation Loss:{:.4f}, Contrastive Loss:{:.4f}, Reconstruction Loss:{:.4f}, Time:{:.4f}".format(global_step,
+                      args.num_steps, loss, losses_tasks[0].item(), losses_tasks[1].item(), losses_tasks[2].item(), time() - t1))
 
             global_step += 1
             if args.distributed:
-                val_cond = (dist.get_rank() == 0) and (global_step % args.eval_num == 0)
+                val_cond = (dist.get_rank() == 0) and (
+                    global_step % args.eval_num == 0)
             else:
                 val_cond = global_step % args.eval_num == 0
 
             if val_cond:
-                val_loss, val_loss_recon, img_list = validation(args, test_loader)
-                writer.add_scalar("Validation/loss_recon", scalar_value=val_loss_recon, global_step=global_step)
-                writer.add_scalar("train/loss_total", scalar_value=np.mean(loss_train), global_step=global_step)
-                writer.add_scalar("train/loss_recon", scalar_value=np.mean(loss_train_recon), global_step=global_step)
+                val_loss, val_loss_recon, img_list = validation(
+                    args, test_loader)
+                writer.add_scalar(
+                    "Validation/loss_recon", scalar_value=val_loss_recon, global_step=global_step)
+                writer.add_scalar(
+                    "train/loss_total", scalar_value=np.mean(loss_train), global_step=global_step)
+                writer.add_scalar(
+                    "train/loss_recon", scalar_value=np.mean(loss_train_recon), global_step=global_step)
 
-                writer.add_image("Validation/x1_gt", img_list[0], global_step, dataformats="HW")
-                writer.add_image("Validation/x1_aug", img_list[1], global_step, dataformats="HW")
-                writer.add_image("Validation/x1_recon", img_list[2], global_step, dataformats="HW")
+                writer.add_image("Validation/x1_gt",
+                                 img_list[0], global_step, dataformats="HW")
+                writer.add_image("Validation/x1_aug",
+                                 img_list[1], global_step, dataformats="HW")
+                writer.add_image("Validation/x1_recon",
+                                 img_list[2], global_step, dataformats="HW")
 
                 if val_loss_recon < val_best:
                     val_best = val_loss_recon
@@ -129,7 +141,8 @@ def main():
                     rots = torch.cat([rot1, rot2], dim=0)
                     imgs_recon = torch.cat([rec_x1, rec_x2], dim=0)
                     imgs = torch.cat([x1, x2], dim=0)
-                    loss, losses_tasks = loss_function(rot_p, rots, contrastive1_p, contrastive2_p, imgs_recon, imgs)
+                    loss, losses_tasks = loss_function(
+                        rot_p, rots, contrastive1_p, contrastive2_p, imgs_recon, imgs)
                 loss_recon = losses_tasks[2]
                 loss_val.append(loss.item())
                 loss_val_recon.append(loss_recon.item())
@@ -138,56 +151,90 @@ def main():
                 xgt = x_gt[0][0][:, :, 48] * 255.0
                 xgt = xgt.astype(np.uint8)
                 x1_augment = x1_augment.detach().cpu().numpy()
-                x1_augment = (x1_augment - np.min(x1_augment)) / (np.max(x1_augment) - np.min(x1_augment))
+                x1_augment = (x1_augment - np.min(x1_augment)) / \
+                    (np.max(x1_augment) - np.min(x1_augment))
                 x_aug = x1_augment[0][0][:, :, 48] * 255.0
                 x_aug = x_aug.astype(np.uint8)
                 rec_x1 = rec_x1.detach().cpu().numpy()
-                rec_x1 = (rec_x1 - np.min(rec_x1)) / (np.max(rec_x1) - np.min(rec_x1))
+                rec_x1 = (rec_x1 - np.min(rec_x1)) / \
+                    (np.max(rec_x1) - np.min(rec_x1))
                 recon = rec_x1[0][0][:, :, 48] * 255.0
                 recon = recon.astype(np.uint8)
                 img_list = [xgt, x_aug, recon]
-                print("Validation step:{}, Loss:{:.4f}, Loss Reconstruction:{:.4f}".format(step, loss, loss_recon))
+                print("Validation step:{}, Loss:{:.4f}, Loss Reconstruction:{:.4f}".format(
+                    step, loss, loss_recon))
 
         return np.mean(loss_val), np.mean(loss_val_recon), img_list
 
     parser = argparse.ArgumentParser(description="PyTorch Training")
-    parser.add_argument("--logdir", default="test", type=str, help="directory to save the tensorboard logs")
-    parser.add_argument("--epochs", default=100, type=int, help="number of training epochs")
-    parser.add_argument("--num_steps", default=100000, type=int, help="number of training iterations")
-    parser.add_argument("--eval_num", default=100, type=int, help="evaluation frequency")
-    parser.add_argument("--warmup_steps", default=500, type=int, help="warmup steps")
-    parser.add_argument("--in_channels", default=1, type=int, help="number of input channels")
-    parser.add_argument("--feature_size", default=48, type=int, help="embedding size")
-    parser.add_argument("--dropout_path_rate", default=0.0, type=float, help="drop path rate")
-    parser.add_argument("--use_checkpoint", action="store_true", help="use gradient checkpointing to save memory")
-    parser.add_argument("--spatial_dims", default=3, type=int, help="spatial dimension of input data")
-    parser.add_argument("--a_min", default=-1000, type=float, help="a_min in ScaleIntensityRanged")
-    parser.add_argument("--a_max", default=1000, type=float, help="a_max in ScaleIntensityRanged")
-    parser.add_argument("--b_min", default=0.0, type=float, help="b_min in ScaleIntensityRanged")
-    parser.add_argument("--b_max", default=1.0, type=float, help="b_max in ScaleIntensityRanged")
-    parser.add_argument("--space_x", default=1.5, type=float, help="spacing in x direction")
-    parser.add_argument("--space_y", default=1.5, type=float, help="spacing in y direction")
-    parser.add_argument("--space_z", default=2.0, type=float, help="spacing in z direction")
-    parser.add_argument("--roi_x", default=96, type=int, help="roi size in x direction")
-    parser.add_argument("--roi_y", default=96, type=int, help="roi size in y direction")
-    parser.add_argument("--roi_z", default=96, type=int, help="roi size in z direction")
-    parser.add_argument("--batch_size", default=2, type=int, help="number of batch size")
-    parser.add_argument("--sw_batch_size", default=2, type=int, help="number of sliding window batch size")
+    parser.add_argument("--logdir", default="test", type=str,
+                        help="directory to save the tensorboard logs")
+    parser.add_argument("--epochs", default=100, type=int,
+                        help="number of training epochs")
+    parser.add_argument("--num_steps", default=100000,
+                        type=int, help="number of training iterations")
+    parser.add_argument("--eval_num", default=100,
+                        type=int, help="evaluation frequency")
+    parser.add_argument("--warmup_steps", default=500,
+                        type=int, help="warmup steps")
+    parser.add_argument("--in_channels", default=1, type=int,
+                        help="number of input channels")
+    parser.add_argument("--feature_size", default=48,
+                        type=int, help="embedding size")
+    parser.add_argument("--dropout_path_rate", default=0.0,
+                        type=float, help="drop path rate")
+    parser.add_argument("--use_checkpoint", action="store_true",
+                        help="use gradient checkpointing to save memory")
+    parser.add_argument("--spatial_dims", default=3, type=int,
+                        help="spatial dimension of input data")
+    parser.add_argument("--a_min", default=-1000, type=float,
+                        help="a_min in ScaleIntensityRanged")
+    parser.add_argument("--a_max", default=1000, type=float,
+                        help="a_max in ScaleIntensityRanged")
+    parser.add_argument("--b_min", default=0.0, type=float,
+                        help="b_min in ScaleIntensityRanged")
+    parser.add_argument("--b_max", default=1.0, type=float,
+                        help="b_max in ScaleIntensityRanged")
+    parser.add_argument("--space_x", default=1.5, type=float,
+                        help="spacing in x direction")
+    parser.add_argument("--space_y", default=1.5, type=float,
+                        help="spacing in y direction")
+    parser.add_argument("--space_z", default=2.0, type=float,
+                        help="spacing in z direction")
+    parser.add_argument("--roi_x", default=96, type=int,
+                        help="roi size in x direction")
+    parser.add_argument("--roi_y", default=96, type=int,
+                        help="roi size in y direction")
+    parser.add_argument("--roi_z", default=96, type=int,
+                        help="roi size in z direction")
+    parser.add_argument("--batch_size", default=2,
+                        type=int, help="number of batch size")
+    parser.add_argument("--sw_batch_size", default=2, type=int,
+                        help="number of sliding window batch size")
     parser.add_argument("--lr", default=4e-4, type=float, help="learning rate")
     parser.add_argument("--decay", default=0.1, type=float, help="decay rate")
     parser.add_argument("--momentum", default=0.9, type=float, help="momentum")
-    parser.add_argument("--lrdecay", action="store_true", help="enable learning rate decay")
-    parser.add_argument("--max_grad_norm", default=1.0, type=float, help="maximum gradient norm")
+    parser.add_argument("--lrdecay", action="store_true",
+                        help="enable learning rate decay")
+    parser.add_argument("--max_grad_norm", default=1.0,
+                        type=float, help="maximum gradient norm")
     parser.add_argument("--loss_type", default="SSL", type=str)
-    parser.add_argument("--opt", default="adamw", type=str, help="optimization algorithm")
+    parser.add_argument("--opt", default="adamw", type=str,
+                        help="optimization algorithm")
     parser.add_argument("--lr_schedule", default="warmup_cosine", type=str)
-    parser.add_argument("--resume", default=None, type=str, help="resume training")
+    parser.add_argument("--resume", default=None,
+                        type=str, help="resume training")
     parser.add_argument("--local_rank", type=int, default=0, help="local rank")
-    parser.add_argument("--grad_clip", action="store_true", help="gradient clip")
-    parser.add_argument("--noamp", action="store_true", help="do NOT use amp for training")
-    parser.add_argument("--dist-url", default="env://", help="url used to set up distributed training")
-    parser.add_argument("--smartcache_dataset", action="store_true", help="use monai smartcache Dataset")
-    parser.add_argument("--cache_dataset", action="store_true", help="use monai cache Dataset")
+    parser.add_argument("--grad_clip", action="store_true",
+                        help="gradient clip")
+    parser.add_argument("--noamp", action="store_true",
+                        help="do NOT use amp for training")
+    parser.add_argument("--dist-url", default="env://",
+                        help="url used to set up distributed training")
+    parser.add_argument("--smartcache_dataset",
+                        action="store_true", help="use monai smartcache Dataset")
+    parser.add_argument("--cache_dataset", action="store_true",
+                        help="use monai cache Dataset")
 
     args = parser.parse_args()
     logdir = "./runs/" + args.logdir
@@ -204,7 +251,8 @@ def main():
     if args.distributed:
         args.device = "cuda:%d" % args.local_rank
         torch.cuda.set_device(args.local_rank)
-        torch.distributed.init_process_group(backend="nccl", init_method=args.dist_url)
+        torch.distributed.init_process_group(
+            backend="nccl", init_method=args.dist_url)
         args.world_size = torch.distributed.get_world_size()
         args.rank = torch.distributed.get_rank()
         print(
@@ -225,13 +273,16 @@ def main():
     model.cuda()
 
     if args.opt == "adam":
-        optimizer = optim.Adam(params=model.parameters(), lr=args.lr, weight_decay=args.decay)
+        optimizer = optim.Adam(params=model.parameters(),
+                               lr=args.lr, weight_decay=args.decay)
 
     elif args.opt == "adamw":
-        optimizer = optim.AdamW(params=model.parameters(), lr=args.lr, weight_decay=args.decay)
+        optimizer = optim.AdamW(params=model.parameters(),
+                                lr=args.lr, weight_decay=args.decay)
 
     elif args.opt == "sgd":
-        optimizer = optim.SGD(params=model.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=args.decay)
+        optimizer = optim.SGD(params=model.parameters(
+        ), lr=args.lr, momentum=args.momentum, weight_decay=args.decay)
 
     if args.resume:
         model_pth = args.resume
@@ -242,14 +293,16 @@ def main():
 
     if args.lrdecay:
         if args.lr_schedule == "warmup_cosine":
-            scheduler = WarmupCosineSchedule(optimizer, warmup_steps=args.warmup_steps, t_total=args.num_steps)
+            scheduler = WarmupCosineSchedule(
+                optimizer, warmup_steps=args.warmup_steps, t_total=args.num_steps)
 
         elif args.lr_schedule == "poly":
 
             def lambdas(epoch):
                 return (1 - float(epoch) / float(args.epochs)) ** 0.9
 
-            scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=lambdas)
+            scheduler = torch.optim.lr_scheduler.LambdaLR(
+                optimizer, lr_lambda=lambdas)
 
     loss_function = Loss(args.batch_size * args.sw_batch_size, args)
     if args.distributed:
@@ -264,8 +317,10 @@ def main():
     else:
         scaler = None
     while global_step < args.num_steps:
-        global_step, loss, best_val = train(args, global_step, train_loader, best_val, scaler)
-    checkpoint = {"epoch": args.epochs, "state_dict": model.state_dict(), "optimizer": optimizer.state_dict()}
+        global_step, loss, best_val = train(
+            args, global_step, train_loader, best_val, scaler)
+    checkpoint = {"epoch": args.epochs, "state_dict": model.state_dict(
+    ), "optimizer": optimizer.state_dict()}
 
     if args.distributed:
         if dist.get_rank() == 0:
--- SwinUNETR/Pretrain/utils/data_utils.py
+++ SwinUNETR/Pretrain/utils/data_utils.py
@@ -28,44 +28,26 @@ from monai.transforms import (
 
 
 def get_loader(args):
-    splits1 = "/dataset_LUNA16_0.json"
-    splits2 = "/dataset_TCIAcovid19_0.json"
-    splits3 = "/dataset_HNSCC_0.json"
-    splits4 = "/dataset_TCIAcolon_v2_0.json"
-    splits5 = "/dataset_LIDC_0.json"
+    splits1 = "./dataset_LUNA16_0.json"
     list_dir = "./jsons"
     jsonlist1 = list_dir + splits1
-    jsonlist2 = list_dir + splits2
-    jsonlist3 = list_dir + splits3
-    jsonlist4 = list_dir + splits4
-    jsonlist5 = list_dir + splits5
-    datadir1 = "/dataset/dataset1"
-    datadir2 = "/dataset/dataset2"
-    datadir3 = "/dataset/dataset3"
-    datadir4 = "/dataset/dataset4"
-    datadir5 = "/dataset/dataset8"
+    datadir1 = "./dataset/dataset1"
     num_workers = 4
-    datalist1 = load_decathlon_datalist(jsonlist1, False, "training", base_dir=datadir1)
+
+    datalist1 = load_decathlon_datalist(
+        jsonlist1, False, "training", base_dir=datadir1)
     print("Dataset 1 LUNA16: number of data: {}".format(len(datalist1)))
+
     new_datalist1 = []
     for item in datalist1:
         item_dict = {"image": item["image"]}
         new_datalist1.append(item_dict)
-    datalist2 = load_decathlon_datalist(jsonlist2, False, "training", base_dir=datadir2)
-    print("Dataset 2 Covid 19: number of data: {}".format(len(datalist2)))
-    datalist3 = load_decathlon_datalist(jsonlist3, False, "training", base_dir=datadir3)
-    print("Dataset 3 HNSCC: number of data: {}".format(len(datalist3)))
-    datalist4 = load_decathlon_datalist(jsonlist4, False, "training", base_dir=datadir4)
-    print("Dataset 4 TCIA Colon: number of data: {}".format(len(datalist4)))
-    datalist5 = load_decathlon_datalist(jsonlist5, False, "training", base_dir=datadir5)
-    print("Dataset 5: number of data: {}".format(len(datalist5)))
-    vallist1 = load_decathlon_datalist(jsonlist1, False, "validation", base_dir=datadir1)
-    vallist2 = load_decathlon_datalist(jsonlist2, False, "validation", base_dir=datadir2)
-    vallist3 = load_decathlon_datalist(jsonlist3, False, "validation", base_dir=datadir3)
-    vallist4 = load_decathlon_datalist(jsonlist4, False, "validation", base_dir=datadir4)
-    vallist5 = load_decathlon_datalist(jsonlist5, False, "validation", base_dir=datadir5)
-    datalist = new_datalist1 + datalist2 + datalist3 + datalist4 + datalist5
-    val_files = vallist1 + vallist2 + vallist3 + vallist4 + vallist5
+
+    vallist1 = load_decathlon_datalist(
+        jsonlist1, False, "validation", base_dir=datadir1)
+
+    datalist = new_datalist1
+    val_files = vallist1
     print("Dataset all training: number of data: {}".format(len(datalist)))
     print("Dataset all validation: number of data: {}".format(len(val_files)))
 
@@ -77,8 +59,10 @@ def get_loader(args):
             ScaleIntensityRanged(
                 keys=["image"], a_min=args.a_min, a_max=args.a_max, b_min=args.b_min, b_max=args.b_max, clip=True
             ),
-            SpatialPadd(keys="image", spatial_size=[args.roi_x, args.roi_y, args.roi_z]),
-            CropForegroundd(keys=["image"], source_key="image", k_divisible=[args.roi_x, args.roi_y, args.roi_z]),
+            SpatialPadd(keys="image", spatial_size=[
+                        args.roi_x, args.roi_y, args.roi_z]),
+            CropForegroundd(keys=["image"], source_key="image", k_divisible=[
+                            args.roi_x, args.roi_y, args.roi_z]),
             RandSpatialCropSamplesd(
                 keys=["image"],
                 roi_size=[args.roi_x, args.roi_y, args.roi_z],
@@ -97,8 +81,10 @@ def get_loader(args):
             ScaleIntensityRanged(
                 keys=["image"], a_min=args.a_min, a_max=args.a_max, b_min=args.b_min, b_max=args.b_max, clip=True
             ),
-            SpatialPadd(keys="image", spatial_size=[args.roi_x, args.roi_y, args.roi_z]),
-            CropForegroundd(keys=["image"], source_key="image", k_divisible=[args.roi_x, args.roi_y, args.roi_z]),
+            SpatialPadd(keys="image", spatial_size=[
+                        args.roi_x, args.roi_y, args.roi_z]),
+            CropForegroundd(keys=["image"], source_key="image", k_divisible=[
+                            args.roi_x, args.roi_y, args.roi_z]),
             RandSpatialCropSamplesd(
                 keys=["image"],
                 roi_size=[args.roi_x, args.roi_y, args.roi_z],
@@ -112,7 +98,8 @@ def get_loader(args):
 
     if args.cache_dataset:
         print("Using MONAI Cache Dataset")
-        train_ds = CacheDataset(data=datalist, transform=train_transforms, cache_rate=0.5, num_workers=num_workers)
+        train_ds = CacheDataset(
+            data=datalist, transform=train_transforms, cache_rate=0.5, num_workers=num_workers)
     elif args.smartcache_dataset:
         print("Using MONAI SmartCache Dataset")
         train_ds = SmartCacheDataset(
@@ -126,7 +113,8 @@ def get_loader(args):
         train_ds = Dataset(data=datalist, transform=train_transforms)
 
     if args.distributed:
-        train_sampler = DistributedSampler(dataset=train_ds, even_divisible=True, shuffle=True)
+        train_sampler = DistributedSampler(
+            dataset=train_ds, even_divisible=True, shuffle=True)
     else:
         train_sampler = None
     train_loader = DataLoader(
@@ -134,6 +122,7 @@ def get_loader(args):
     )
 
     val_ds = Dataset(data=val_files, transform=val_transforms)
-    val_loader = DataLoader(val_ds, batch_size=args.batch_size, num_workers=num_workers, shuffle=False, drop_last=True)
+    val_loader = DataLoader(val_ds, batch_size=args.batch_size,
+                            num_workers=num_workers, shuffle=False, drop_last=True)
 
     return train_loader, val_loader
